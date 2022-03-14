c521181fbba8bb2ff7eeaf245ba9ebec788b1d1d
9nightwish9 <101181561+9nightwish9@users.noreply.github.com>
2022-03-14 11:01:43 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,4 +2,4 @@ print("Hoş Geldiniz")
 a=int(input("Alacağınız Tv'nin ücretini giriiniz-> "))
 b=int(input("yüzdeliğini giriniz->"))
 k=a*b/100
-print("kazancınız şu kadardır->{}",k)
+print("kazancınız şu kadardır->",k)
